f78fb978fe3977530ea4ffca95e0c3d22c1e2701
Oliver Gorwits <oliver@cpan.org>
2022-05-20 20:02:17 +0100

    add wp-content

---
+++ wp-links-opml.php
@@ -0,0 +1,22 @@
+<?xml version="1.0"?>
+<opml version="1.0">
+	<head>
+		<title>Links for Cats and Code</title>
+		<dateCreated>Sat, 29 Mar 2014 23:30:22 GMT</dateCreated>
+		<!-- generator="WordPress/3.6.1" -->
+	</head>
+	<body>
+<outline type="category" title="Blogroll">
+	<outline text="Documentation" type="link" xmlUrl="" htmlUrl="http://codex.wordpress.org/" updated="" />
+	<outline text="Plugins" type="link" xmlUrl="" htmlUrl="http://wordpress.org/extend/plugins/" updated="" />
+	<outline text="Suggest Ideas" type="link" xmlUrl="" htmlUrl="http://wordpress.org/extend/ideas/" updated="" />
+	<outline text="Support Forum" type="link" xmlUrl="" htmlUrl="http://wordpress.org/support/" updated="" />
+	<outline text="Themes" type="link" xmlUrl="" htmlUrl="http://wordpress.org/extend/themes/" updated="" />
+	<outline text="WordPress Blog" type="link" xmlUrl="http://wordpress.org/news/feed/" htmlUrl="http://wordpress.org/news/" updated="" />
+	<outline text="WordPress Planet" type="link" xmlUrl="" htmlUrl="http://planet.wordpress.org/" updated="" />
+</outline>
+<outline type="category" title="Contributors">
+	<outline text="Oliver Gorwits" type="link" xmlUrl="http://blogs.perl.org/users/oliver_gorwits/atom.xml" htmlUrl="http://blogs.perl.org/users/oliver_gorwits/" updated="" />
+</outline>
+</body>
+</opml>
